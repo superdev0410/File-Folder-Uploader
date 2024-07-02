afb2a268f4e5fde4f34940feefd4a6680fcd5871
superdev0410 <superdev0410@protonmail.com>
2024-07-02 14:51:06 -0700

    complete api endpoints for file upload and download

---
+++ src/server/routes/file.tsx
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./files");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const fileRouter = Router();
+
+fileRouter.post("/upload", upload.array("files"), async (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+fileRouter.get("/download/:file", async (req: Request, res: Response) => {
+  const { file } = req.params;
+  res.download(`./files/${file}`);
+});
